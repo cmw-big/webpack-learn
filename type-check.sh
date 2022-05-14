fa3dcb0bfdb41bcca92738bcc722051a7c4dbe44
程明旺 <cmw1002113844@gmail.com>
2022-05-14 17:47:59 +0800

    fix: 修复问题

--- type-check.sh
+++ type-check.sh
@@ -3,12 +3,12 @@
 # 执行类型检查
 for file in ./packages/*; do
     if test -d "$file"; then
-        pnpm exec tsc --noEmit -p "$file"
+        pnpm exec tsc -p "$file"
     fi
 done
 
 for file in ./libs/*; do
     if test -d "$file"; then
-        pnpm exec tsc --noEmit -p "$file"
+        pnpm exec tsc -p "$file"
     fi
 done
